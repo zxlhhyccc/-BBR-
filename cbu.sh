1fda392e2e5feb359e02b3de45c95f99263a0a9f
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2018-12-02 19:50:20 +0800

    Update cbu.sh

--- cbu.sh
+++ cbu.sh
@@ -438,7 +438,6 @@ detele_kernel(){
 			done
 			echo -e "内核卸载完毕，继续..."
 		fi
-	fi
 	elif [[ "${release}" == "ubuntu" ]]; then
 		deb-head_total=`dpkg -l | grep linux-headers | awk '{print $2}' | grep -v "${kernel_version}" | wc -l`
 		if [ "${deb-head_total}" > "1" ]; then

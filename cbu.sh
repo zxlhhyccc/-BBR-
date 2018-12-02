973c85799d560b44b1d4798644162bb1d1b45d5a
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2018-12-02 19:15:01 +0800

    Update cbu.sh

--- cbu.sh
+++ cbu.sh
@@ -431,9 +431,11 @@ detele_kernel(){
 		if [ "${deb_total}" > "1" ]; then
 			echo -e "检测到 ${deb_total} 个其余内核，开始卸载..."
 			for((integer = 1; integer <= ${deb_total}; integer++)); do
-				deb_del=`dpkg -l|grep linux-image |grep linux-headers | awk '{print $2}' | grep -v "${kernel_version}" | head -${integer}`
+				deb_del=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "${kernel_version}" | head -${integer}`
+				deb-head_del=`dpkg -l|grep linux-headers | awk '{print $2}' | grep -v "${kernel_version}" | head -${integer}`
 				echo -e "开始卸载 ${deb_del} 内核..."
 				apt-get purge -y ${deb_del}
+				apt-get purge -y ${deb-head_del}
 				echo -e "卸载 ${deb_del} 内核卸载完成，继续..."
 			done
 			echo -e "内核卸载完毕，继续..."

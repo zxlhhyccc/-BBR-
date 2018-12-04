5353b659f871ebc663638911697ef7f8f5246c23
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2018-12-04 13:18:53 +0800

    Update bbr.sh

--- bbr/bbr.sh
+++ bbr/bbr.sh
@@ -29,9 +29,9 @@ installbbr(){
 		yum install -y http://${github}/bbr/${release}/${version}/${bit}/kernel-ml-devel-${kernel_version}.rpm
 	elif [[ "${release}" == "debian" || "${release}" == "ubuntu" ]]; then
 		mkdir bbr && cd bbr
-		wget -N --no-check-certificate linux-headers_all.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-headers-4.9.142_all.deb
-		wget -N --no-check-certificate linux-headers_amd64.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-headers-4.9.142-generic_amd64.deb
-		wget -N --no-check-certificate linux-image-generic_amd64.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-image-4.9.142-generic_amd64.deb	
+		wget -N --no-check-certificate -O linux-headers_all.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-headers-4.9.142_all.deb
+		wget -N --no-check-certificate -O linux-headers_amd64.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-headers-4.9.142-generic_amd64.deb
+		wget -N --no-check-certificate -O linux-image-generic_amd64.deb https://github.com/zxlhhyccc/debian9/raw/master/linux-image-4.9.142-generic_amd64.deb	
 		dpkg -i linux-headers_all.deb
 		dpkg -i linux-headers_amd64.deb
 		dpkg -i linux-image-generic_amd64.deb
